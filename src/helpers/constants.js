8e1a1e671afa5fd7108421cdad27afd5771f8bd0
Darwin <darwinfegarido@gmail.com>
2023-03-23 15:41:42 +0800

    Upgrade to react 18 and update landing page

---
+++ src/helpers/constants.js
@@ -0,0 +1,9 @@
+
+/* COLORS */
+export const WHITE = '#dfdfdf'
+export const LIME = '#76c219'
+
+/* FONTS */
+export const FONT_FAMILY = 'Montserrat, sans-serif'
+
+
--- src/layout/Body.jsx
+++ src/layout/Body.jsx
@@ -6,6 +6,11 @@ import Portfolio from "../pages/Portfolio"
 import Contact from "../pages/Contact"
 import { useEffect, useRef } from "react"
 import { setReference } from "../store/metadata"
+import { 
+    WHITE, 
+    LIME, 
+    FONT_FAMILY } from '../helpers/constants'
+import Button from '../components/atom/Button'
 
 
 const Body = () => {
@@ -18,41 +23,47 @@ const Body = () => {
         dispatch(setReference({name: 'homeRef', value: homeRef }))
     })
 
+    const greetings = "Hi i'm"
+    const name = "Darwin Fegarido"
+
     const nameToAnimate = [
-        "Darwin Fegarido",
         "Javascript Developer",
         "Python Developer",
         "Frontend Developer",
         "Backend Developer",
         "Solution Architect",
-        "UI / UX Designer", 
+        "DevOps", 
         "Shopify Developer",
+        "Fullstack Developer",
     ]
 
     return (
         <div 
             ref={homeRef}
-            className="flex-1 flex-row "
-        >
+            className="flex-1 flex-row ">
             {/* Landing Page Background */}
             <div 
                 className={`bg-cover bg-no-repeat bg-center bg-fixed bg-landing-two flex min-w-full`} 
                 style={{ 
                     height: `${windowHeight}px` ,
-                }}
-            >
+                }}>
                 <div 
                     style={{
                         ...styles.container,
                         ...(scrollTop > 100 ? styles.hideContainer : null),
                     }}
-                    className={`flex-1 grid justify-items-center`}
-                >
-                    <div  style={styles.hello} className={`flex-1`}>
-                        HELLO
+                    className={`flex-1 grid justify-items-center`}>
+                    <div  style={styles.greetings} className={`flex-1`}>
+                        { greetings }
+                    </div>
+                    <div  style={styles.name} className={`flex-1`}>
+                        { name }
                     </div>
                     <div className="flex-1"  style={styles.iAm}>
-                        <p>I am <CharacterAnimation label={nameToAnimate}/> </p>
+                        <p> <CharacterAnimation label={nameToAnimate}/> </p>
+                    </div>
+                    <div  style={styles.contactBtn} className={`flex-1`}>
+                        <Button label={"Contact"} style={styles.contactLabel} className={`h-10`}  />
                     </div>
                 </div>
                 
@@ -77,30 +88,56 @@ const styles = {
     hideContainer: {
         opacity: '0'
     },
-    hello: {
+    greetings: {
         position: 'absolute',
         top: '15rem',
-        fontFamily: 'Nunito Sans',
+        left: '100px',
+        fontFamily: FONT_FAMILY,
         fontStyle: 'normal',
         fontWeight: '800',
-        fontSize: '3rem',
+        fontSize: '4.5rem',
         lineHeight: '82px',
-        color: '#FFFFFF',
+        color: WHITE,
+    },
+    name: {
+        position: 'absolute',
+        top: '19rem',
+        left: '100px',
+        fontFamily: FONT_FAMILY,
+        fontStyle: 'normal',
+        fontWeight: '800',
+        fontSize: '3.5rem',
+        lineHeight: '82px',
+        color: WHITE,
     },
     iAm: {
         position: 'absolute',
         height: '68px',
-        top: '303px',
+        top: '23rem',
+        left: '100px',
 
-        fontFamily: 'Nunito Sans',
+        fontFamily: FONT_FAMILY,
         fontStyle: 'normal',
         fontWeight: '300',
         fontSize: '2rem',
         lineHeight: '68px',
 
-        color: '#FFFFFF',
+        color: WHITE,
 
     },
+    contactBtn: {
+        position: 'absolute',
+        height: '68px',
+        width: '200px',
+        top: '29rem',
+        left: '100px',
+        color: WHITE,
+    },
+    contactLabel: {
+        fontSize: '21px',
+        fontWeight: '600',
+        color: LIME
+    }
 }
 
 export default Body;
