c36b60865d042ef1dc42cb0d5989aa4c6537ce5b
Darwin <darwinfegarido@gmail.com>
2023-03-23 15:41:42 +0800

    Upgrade to react 18 and update landing page

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import store from './store'
 import { Provider } from 'react-redux'
 import Default from './layout/Default';
 
 
-
-
-ReactDOM.render(
+const container = document.getElementById('root')
+const root = createRoot(container)
+root.render(
   <Provider store={store}>
     <Default />
-  </Provider>,
-  document.getElementById('root')
+  </Provider>
 );
 
